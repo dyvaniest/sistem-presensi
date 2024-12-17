5e04b48d8a11c5baf84ab58ec69fd7f38be0569f
dyvaniest <divanypangestika@gmail.com>
2024-12-17 07:57:41 +0700

    edit main 2

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 	"sistem-presensi/models"
 
 	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
+	// "github.com/joho/godotenv"
 	"github.com/rs/cors"
 )
 
@@ -22,10 +22,6 @@ import (
 // }
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
 
 	// Connect to Database
 	dbInstance := db.NewDB()
